0d2e0bfff19bf62225b80e773642d180d5e064db
Ximen <Cheng.Ximen@bci.ca>
2023-02-01 11:59:16 -0800

    fix total fund module, scale function

--- lib/total_fund_tree.py
+++ lib/total_fund_tree.py
@@ -151,17 +151,19 @@ def update_GPF_scale_calc(from_date: date, to_date: date) -> None:
     with xw.App(visible=False) as app:
         wb = app.books.open(file_path)
         sheet = wb.sheets[0]
-        num_of_positions = sheet.range("A3").end("down").row - 2
-        sheet.range("A3").expand("down").value = [
+        row_start_mv = 3
+        row_start_pv = 18
+        num_of_positions = sheet.range(f"A{row_start_mv}").end("down").row - 2
+        sheet.range(f"A{row_start_mv}").expand("down").value = [
             [datetime(to_date.year, to_date.month, to_date.day)]]*num_of_positions
-        sheet.range("A16").expand("down").value = [
+        sheet.range(f"A{row_start_pv}").expand("down").value = [
             [datetime(to_date.year, to_date.month, to_date.day)]]*num_of_positions
-        for r in range(3, 3+num_of_positions):
+        for r in range(row_start_mv, row_start_mv+num_of_positions):
             sheet.range(f"H{r}").value = gpf_mv.loc[sheet.range(
                 f"E{r}").value, "BASE_Total_Market_Value"]
             sheet.range(f"I{r}").value = gpf_mv.loc[sheet.range(
                 f"E{r}").value, "FX_RATE"]
-        for r in range(18, 18+num_of_positions):
+        for r in range(row_start_pv, row_start_pv+num_of_positions):
             sheet.range(f"E{r}").value = pv_data.loc[sheet.range(
                 f"D{r}").value, "PV"]
         app.calculate()
