f4f2b67b7d1a50ef840db9e0484f2db9f016d2b5
Ximen <Cheng.Ximen@bci.ca>
2023-09-20 11:22:28 -0700

    update repo sql add -3

--- lib/sql/repo/repo2.sql
+++ lib/sql/repo/repo2.sql
@@ -1,4 +1,4 @@
-SELECT FORMAT(CONVERT(DATE,AsOfDate),'yyyyMMdd') [Valuation_date], CUSIP [BCI_underlyingCUSIP], Counterparty [counterparty], PARValue [BCI_underlyingParAmount]
+SELECT (FORMAT(CONVERT(DATE,AsOfDate),'yyyyMMdd') - 3) [Valuation_date], CUSIP [BCI_underlyingCUSIP], Counterparty [counterparty], PARValue [BCI_underlyingParAmount]
 ,'Repurchase Agreement' [InstrumentType],'REPO' [InstrumentTypeCode]
 FROM [RiskRaw].[rpt_DerivativeOp].[VW_FundingDeskAggregatedCollateral]
 WHERE AsOfDate = '?'
